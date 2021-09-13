b1aa517fdd3ac7629002f065a422bcf3ed50d674
Hyunjining <120hyunjin@naver.com>
2021-09-13 17:10:17 +0900

    #2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import Potato from './Potato';
+
+
+function Food(props) {
+  return <h1>I like {props.fav}</h1>
+}
 
 function App() {
   return (
     <div>
       <h1>Hello~ </h1>
-      <Potato />
+      <Food fav="kimchi" />
+      <Food fav="ramen" />
+      <Food fav="samgiopsal" />
+      <Food fav="chukumi" />
     </div>
   );
 }
